9ce19a53114e74c2c603f1bd7f6d7512e90f83e7
dev-rb <rbatra486@gmail.com>
2024-01-04 23:14:00 -0500

    Update tab styles. Update right status battery info and spacing. Update window action icons.

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+local icons = wezterm.nerdfonts
+
+local function space(string, repLeft, repRight)
+  repLeft = repLeft or 1
+  repRight = repRight or 1
+  return string.rep(" ", repLeft).. string .. string.rep(" ", repRight)
+end
+
 -- This is where you actually apply your config choices
 config.scrollback_lines = 3500
 -- For example, changing the color scheme:
@@ -53,25 +61,30 @@ config.adjust_window_size_when_changing_font_size = false
 config.front_end = "OpenGL"
 config.use_fancy_tab_bar = false
 -- config.tab_bar_at_bottom = true
-local window_min = " - "
-local window_max = " + "
-local window_close = " x "
+local window_min = space(icons.fae_minimize, 2, 2)
+local window_max = space(icons.fae_maximize, 2, 2)
+local window_close = space(icons.fae_thin_close, 2, 2)
 config.tab_bar_style = {
 	window_hide = window_min,
 	window_hide_hover = window_min,
 	window_maximize = window_max,
 	window_maximize_hover = window_max,
 	window_close = window_close,
-	window_close_hover = window_close,
+	window_close_hover = wezterm.format{
+    {Background = {Color = "red"}},
+    {Text = window_close}
+  }
 }
 config.font_size = 10
+local color_bg = wezterm.color.parse("#1D61D9")
+local color_fg = wezterm.color.parse("#1DA0D8")
 config.colors = {
 	tab_bar = {
-		background = "#000",
+		background = "transparent",
 
 		active_tab = {
-			fg_color = "#ffffff",
-			bg_color = "#111111",
+			fg_color = color_fg:lighten(0.8),
+			bg_color = color_bg:lighten(0.03),
 		},
 
 		new_tab = {
@@ -85,24 +98,88 @@ config.colors = {
 		},
 	},
 }
-wezterm.on("format-tab-title", function(tab)
-	local tab_index = string.format("  %s  ", tab.tab_index + 1)
+
+config.window_frame = {
+  active_titlebar_bg = color_bg,
+  inactive_titlebar_bg = color_bg
+}
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+
+  if title and #title > 0 then
+
+    return string.match(title, "(%w+)/?$")
+  end
+
+  return tab_info.active_pane.current_working_dir:gsub("%.exe", ""):match("(%w+)/?$")
+end
+
+wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
+	local title = tab_title(tab)
+  title = wezterm.truncate_right(title, max_width - 3)
+  local bg = color_bg:darken(0.7)
+  local fg = color_fg
+
+  if tab.is_active then
+    bg = bg:lighten(0.3)
+    fg = fg:lighten(1)
+  end
+  if hover then
+    bg = bg:lighten(0.1)
+    fg = fg:lighten(0.1)
+  end
+
+  local edge_fg = bg
+
 	return wezterm.format({
-		-- { Text = "▏" },
-		{ Text = tab_index },
+    {Attribute = {Italic = false}},
+    {Attribute = { Intensity = "Normal"}},
+    {Background = {Color = "transparent"}},
+    {Foreground = {Color = edge_fg}},
+		{ Text = "" },
+    {Background = {Color = bg}},
+    {Foreground = {Color = fg}},
+		{ Text = title },
+    {Background = {Color = "transparent"}},
+    {Foreground = {Color = edge_fg}},
+		{ Text = " " },
 	})
 end)
+
 
 -- config.hide_tab_bar_if_only_one_tab = true
 wezterm.on("update-right-status", function(window, pane)
 	local user_vars = pane:get_user_vars()
 
-	local date = " " .. wezterm.strftime("%r | %m-%d-%Y") .. " "
+	local date = " " .. wezterm.strftime("%r  %m/%d/%Y") .. " "
+  local bat = ''
+  for _, b in ipairs(wezterm.battery_info()) do
+    local raw_charge = b.state_of_charge * 100
+    local charge_str = string.format('%.0f', raw_charge)
+    local trunc_charge = tonumber(charge_str)
+    local multiple = math.floor(trunc_charge / 10) * 10
+    local charging = b.state == "Charging"
+
+    local icon_name = "md_battery"
+
+    if trunc_charge ~= 100 then
+      icon_name = icon_name .. (charging and "_charging" or "") .. "_" .. tostring(multiple)
+    else
+      if charging then
+        icon_name = icon_name .. "_charging_100"
+      end
+    end
+
+    local icon = icons[icon_name]
+
+    bat = icon .. " " .. charge_str .. "%"
+  end
+
 
 	-- figure out a way to center it
 	window:set_right_status(wezterm.format({
-		{ Background = { Color = "#000" } },
-		{ Text = date },
+		{ Background = { Color = "transparent" } },
+		{ Text = bat..' '..date },
 	}))
 end)
 
