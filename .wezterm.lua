fa3ccb1f2badc2ffd08252eb0bdb2b880643696c
dev-rb <rbatra486@gmail.com>
2024-01-08 14:01:15 -0500

    Formatting

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,3 @@
--- -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
@@ -13,15 +12,21 @@ end
 local icons = wezterm.nerdfonts
 
 local function space(string, repLeft, repRight)
-  repLeft = repLeft or 1
-  repRight = repRight or 1
-  return string.rep(" ", repLeft).. string .. string.rep(" ", repRight)
+	repLeft = repLeft or 1
+	repRight = repRight or 1
+	return string.rep(" ", repLeft) .. string .. string.rep(" ", repRight)
 end
 
 -- This is where you actually apply your config choices
 config.scrollback_lines = 3500
 -- For example, changing the color scheme:
 config.color_scheme = "Argonaut (Gogh)"
+-- Fonts:
+-- Monaspace Neon
+-- JetBrainsMono Nerd Font Mono
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 10
+config.line_height = 1.1
 -- config.default_gui_startup_args = { 'connect', 'unix' }
 -- config.default_prog = { 'wsl' }
 -- config.unix_domains = {
@@ -40,12 +45,11 @@ config.window_padding = {
 	right = 0,
 }
 
-
-config.default_domain = 'WSL:Ubuntu'
-config.window_background_image = 'D:/Desktop/Pictures/wallhaven-q6532q.png'
+config.default_domain = "WSL:Ubuntu"
+config.window_background_image = "D:/Desktop/Pictures/Wallpapers/wallhaven-vq3xd3.jpg"
 config.window_background_image_hsb = {
 	-- Darken the background image by reducing it to 1/3rd
-	brightness = 0.2,
+	brightness = 0.1,
 
 	-- You can adjust the hue by scaling its value.
 	-- a multiplier of 1.0 leaves the value unchanged.
@@ -58,7 +62,7 @@ config.window_background_opacity = 1
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.adjust_window_size_when_changing_font_size = false
 -- config.win32_system_backdrop = "Acrylic"
-config.front_end = "OpenGL"
+config.front_end = "WebGpu"
 config.use_fancy_tab_bar = false
 -- config.tab_bar_at_bottom = true
 local window_min = space(icons.fae_minimize, 2, 2)
@@ -70,10 +74,10 @@ config.tab_bar_style = {
 	window_maximize = window_max,
 	window_maximize_hover = window_max,
 	window_close = window_close,
-	window_close_hover = wezterm.format{
-    {Background = {Color = "red"}},
-    {Text = window_close}
-  }
+	window_close_hover = wezterm.format({
+		{ Background = { Color = "red" } },
+		{ Text = window_close },
+	}),
 }
 config.font_size = 10
 local color_bg = wezterm.color.parse("#1D61D9")
@@ -100,86 +104,83 @@ config.colors = {
 }
 
 config.window_frame = {
-  active_titlebar_bg = color_bg,
-  inactive_titlebar_bg = color_bg
+	active_titlebar_bg = "transparent",
+	inactive_titlebar_bg = "transparent",
 }
 local function tab_title(tab_info)
-  local title = tab_info.tab_title
+	local title = tab_info.tab_title
 
-  if title and #title > 0 then
+	if title and #title > 0 then
+		return string.match(title, "(%w+)/?$")
+	end
 
-    return string.match(title, "(%w+)/?$")
-  end
-
-  return tab_info.active_pane.current_working_dir:gsub("%.exe", ""):match("(%w+)/?$")
+	return tab_info.active_pane.current_working_dir:gsub("%.exe", ""):match("(%w+)/?$")
 end
 
 wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
 	local title = tab_title(tab)
-  title = wezterm.truncate_right(title, max_width - 3)
-  local bg = color_bg:darken(0.7)
-  local fg = color_fg
+	title = wezterm.truncate_right(title, max_width - 3)
+	local bg = color_bg:darken(0.7)
+	local fg = color_fg
 
-  if tab.is_active then
-    bg = bg:lighten(0.3)
-    fg = fg:lighten(1)
-  end
-  if hover then
-    bg = bg:lighten(0.1)
-    fg = fg:lighten(0.1)
-  end
+	if tab.is_active then
+		bg = bg:lighten(0.3)
+		fg = fg:lighten(1)
+	end
+	if hover then
+		bg = bg:lighten(0.1)
+		fg = fg:lighten(0.1)
+	end
 
-  local edge_fg = bg
+	local edge_fg = bg
 
 	return wezterm.format({
-    {Attribute = {Italic = false}},
-    {Attribute = { Intensity = "Normal"}},
-    {Background = {Color = "transparent"}},
-    {Foreground = {Color = edge_fg}},
+		{ Attribute = { Italic = false } },
+		{ Attribute = { Intensity = "Normal" } },
+		{ Background = { Color = "transparent" } },
+		{ Foreground = { Color = edge_fg } },
 		{ Text = "" },
-    {Background = {Color = bg}},
-    {Foreground = {Color = fg}},
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
 		{ Text = title },
-    {Background = {Color = "transparent"}},
-    {Foreground = {Color = edge_fg}},
+		{ Background = { Color = "transparent" } },
+		{ Foreground = { Color = edge_fg } },
 		{ Text = " " },
 	})
 end)
-
 
 -- config.hide_tab_bar_if_only_one_tab = true
 wezterm.on("update-right-status", function(window, pane)
 	local user_vars = pane:get_user_vars()
 
 	local date = " " .. wezterm.strftime("%r  %m/%d/%Y") .. " "
-  local bat = ''
-  for _, b in ipairs(wezterm.battery_info()) do
-    local raw_charge = b.state_of_charge * 100
-    local charge_str = string.format('%.0f', raw_charge)
-    local trunc_charge = tonumber(charge_str)
-    local multiple = math.floor(trunc_charge / 10) * 10
-    local charging = b.state == "Charging"
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		local raw_charge = b.state_of_charge * 100
+		local charge_str = string.format("%.0f", raw_charge)
+		local trunc_charge = tonumber(charge_str)
+		local multiple = math.floor(trunc_charge / 10) * 10
+		local charging = b.state == "Charging"
 
-    local icon_name = "md_battery"
+		local icon_name = "md_battery"
 
-    if trunc_charge ~= 100 then
-      icon_name = icon_name .. (charging and "_charging" or "") .. "_" .. tostring(multiple)
-    else
-      if charging then
-        icon_name = icon_name .. "_charging_100"
-      end
-    end
+		if trunc_charge ~= 100 then
+			icon_name = icon_name .. (charging and "_charging" or "") .. "_" .. tostring(multiple)
+		else
+			if charging then
+				icon_name = icon_name .. "_charging_100"
+			end
+		end
 
-    local icon = icons[icon_name]
+		local icon = icons[icon_name]
 
-    bat = icon .. " " .. charge_str .. "%"
-  end
-
+		bat = icon .. " " .. charge_str .. "%"
+	end
 
 	-- figure out a way to center it
 	window:set_right_status(wezterm.format({
 		{ Background = { Color = "transparent" } },
-		{ Text = bat..' '..date },
+		{ Text = bat .. " " .. date },
 	}))
 end)
 
