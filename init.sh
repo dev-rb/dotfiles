209ae1ae0415482f45457a32d66401d256c8fcfd
dev-rb <rbatra486@gmail.com>
2024-08-05 11:16:12 -0400

    Add home manager install and setup flake to init script

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 pkgs=(zsh curl)
 sudo apt install "${pkgs[@]}" 
 curl -L https://nixos.org/nix/install | sh -s -- --daemon
+nix-shell '<home-manager>' -A install
+
+home-manager switch --flake home-manager/#dev-rb
 
 ln -s .zsh/.zshrc ~/.zshrc
