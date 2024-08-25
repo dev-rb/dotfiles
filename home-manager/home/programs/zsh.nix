ab1291467aa07fc1546a0fea5e9e15cd2cd5be41
dev-rb <rbatra486@gmail.com>
2024-08-25 01:21:04 -0400

    Update zsh keymaps

--- home-manager/home/programs/zsh.nix
+++ home-manager/home/programs/zsh.nix
@@ -7,22 +7,29 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     plugins = [
-     # {
-     #    name = "fzf-tab";
-     #    src = pkgs.fetchFromGitHub {
-     #      owner = "Aloxaf";
-     #      repo = "fzf-tab";
-     #      rev = "v1.1.2";
-     #      sha256 = "7fed01afba9392b6392408b9a0cf888522ed7a10";
-     #    };
-     #  }
+      # {
+      #    name = "fzf-tab";
+      #    src = pkgs.fetchFromGitHub {
+      #      owner = "Aloxaf";
+      #      repo = "fzf-tab";
+      #      rev = "v1.1.2";
+      #      sha256 = "7fed01afba9392b6392408b9a0cf888522ed7a10";
+      #    };
+      #  }
     ];
     shellAliases = {
       ls = "eza --icons=always";
       cat = "bat";
     };
 
-    initExtra = ''eval "$(oh-my-posh init zsh --config ~/dotfiles/pure.omp.json)"'';
+    defaultKeymap = "emacs";
+
+    initExtra = ''
+      eval "$(oh-my-posh init zsh --config ~/dotfiles/pure.omp.json)"
+      bindkey '^p' history-search-backward
+      bindkey '^n' history-search-forward
+
+    '';
   };
 
   programs.oh-my-posh = {
