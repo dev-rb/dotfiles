dad9509350971a6995b493a0b9ccc671ec6056c9
dev-rb <rbatra486@gmail.com>
2024-09-01 19:05:07 -0400

    Update telescope config in lazy. Add ui-select extension for code actions

--- home-manager/home/programs/nixvim/config/plugins/lazy.nix
+++ home-manager/home/programs/nixvim/config/plugins/lazy.nix
@@ -180,8 +180,104 @@ in
         }
         {
           pkg = telescope-nvim;
-          opts = opts.telescope;
-          dependencies = [ { pkg = telescope-live-grep-args-nvim; } ];
+          config = ''
+            function(_, opts)
+              opts = vim.tbl_deep_extend("force", opts, {
+                extensions = {
+                  ["ui-select"] = {
+                    require('telescope.themes').get_cursor {}
+                  }
+                }
+              })
+
+              require('telescope').setup(opts)
+                  require('telescope').load_extension("ui-select")
+            end
+          '';
+          opts = {
+            pickers = {
+              find_files = {
+                hidden = false;
+              };
+            };
+            defaults = {
+              prompt_prefix = "   ";
+              selection_caret = "  ";
+              entry_prefix = "  ";
+              initial_mode = "insert";
+              selection_strategy = "reset";
+              sorting_strategy = "ascending";
+              layout_strategy = "horizontal";
+              layout_config = {
+                horizontal = {
+                  prompt_position = "top";
+                  preview_width = 0.55;
+                  results_width = 0.8;
+                };
+                vertical = {
+                  mirror = false;
+                };
+                width = 0.87;
+                height = 0.8;
+                preview_cutoff = 120;
+              };
+              #file_sorter = "require(' telescope.sorters ').get_fuzzy_file";
+              #file_ignore_patterns = { "node_modules" };
+              #generic_sorter = "require(' telescope.sorters ').get_generic_fuzzy_sorter";
+              #path_display = { "truncate" };
+              winblend = 0;
+              border = { };
+              borderchars = [
+                "─"
+                "│"
+                "─"
+                "│"
+                "╭"
+                "╮"
+                "╯"
+                "╰"
+              ];
+              color_devicons = true;
+              # set_env.COLORTERM = "truecolor";
+              set_env = {
+                "COLORTERM" = "truecolor";
+              };
+              #file_previewer = "require(' telescope.previewers ').vim_buffer_cat.new";
+              #grep_previewer = "require(' telescope.previewers ').vim_buffer_vimgrep.new";
+              #qflist_previewer = "require(' telescope.previewers ').vim_buffer_qflist.new";
+              #buffer_previewer_maker = "require(' telescope.previewers ').buffer_previewer_maker";
+              #mappings = {
+              #n = { ["q"] = require("telescope.actions").close };
+              #};
+            };
+            # keymaps = {
+            #   "<leader>fh" = {
+            #     mode = "n";
+            #     action = "help_tags";
+            #     options = {
+            #       desc = "[F]ind [H]elp";
+            #     };
+            #   };
+            #   "<leader>fk" = {
+            #     mode = "n";
+            #     action = "keymaps";
+            #     options = {
+            #       desc = "[F]ind [K]eymaps";
+            #     };
+            #   };
+            #   "<leader>fr" = {
+            #     mode = "n";
+            #     action = "resume";
+            #     options = {
+            #       desc = "[F]ind [R]esume";
+            #     };
+            #   };
+            # };
+          };
+          dependencies = [
+            { pkg = telescope-live-grep-args-nvim; }
+            { pkg = telescope-ui-select-nvim; }
+          ];
         }
         {
           pkg = which-key-nvim;
