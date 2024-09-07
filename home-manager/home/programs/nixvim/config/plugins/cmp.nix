8584c30d10250d549c986a36eb8f406317771f86
dev-rb <rbatra486@gmail.com>
2024-09-07 14:19:22 -0400

    Update cmp to add completion for '/' search.

--- home-manager/home/programs/nixvim/config/plugins/cmp.nix
+++ home-manager/home/programs/nixvim/config/plugins/cmp.nix
@@ -1,29 +1,50 @@
 {
-  programs.nixvim.plugins = {
-    luasnip.enable = true;
-    cmp = {
-      enable = true;
-      settings = {
-        snippet.expand.__raw = # lua
-          ''
-            function(args)
-              require('luasnip').lsp_expand(args.body)
-            end
-          '';
-        mapping = {
-          "<C-n>" = "require('cmp').mapping.select_next_item()";
-          "<C-p>" = "require('cmp').mapping.select_prev_item()";
-          "<C-Space>" = "require('cmp').mapping.complete()";
-          "<C-e>" = "require('cmp').mapping.abort()";
-          "<CR>" = "require('cmp').mapping.confirm({ select = true })";
+  programs.nixvim = {
+    extraConfigLua = ''
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+    '';
+
+    plugins = {
+      luasnip.enable = true;
+      cmp = {
+        enable = true;
+        settings = {
+          snippet.expand.__raw = # lua
+            ''
+              function(args)
+                require('luasnip').lsp_expand(args.body)
+              end
+            '';
+          mapping = {
+            "<C-n>" = "require('cmp').mapping.select_next_item()";
+            "<C-p>" = "require('cmp').mapping.select_prev_item()";
+            "<C-Space>" = "require('cmp').mapping.complete()";
+            "<C-e>" = "require('cmp').mapping.abort()";
+            "<CR>" = "require('cmp').mapping.confirm({ select = true })";
+            "<Tab>" = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                else
+                  fallback()
+                end 
+              end'';
+          };
+          autoEnableSources = true;
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "cmdline"; }
+          ];
+          #formatting.format.__raw = "require('cmp-tailwind-colors').format";
         };
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "luasnip"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
-        #formatting.format.__raw = "require('cmp-tailwind-colors').format";
       };
     };
   };
