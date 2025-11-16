9f2333fa34196f5fd03dcb1f497f6e9ecd4a005f
dev-rb <rbatra486@gmail.com>
2025-11-15 19:05:02 -0500

    Remove manual base46 plugin add

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,11 +21,6 @@ require("lazy").setup({
     branch = "v2.5",
     import = "nvchad.plugins",
   },
-  {
-    "NvChad/base46",
-    lazy = false,
-    branch = "v3.0",
-  },
 
   { import = "plugins" },
 }, lazy_config)
