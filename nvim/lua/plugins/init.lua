b777eb99d07e70662dd5ab60d7d5c2760ab92a70
dev-rb <rbatra486@gmail.com>
2025-11-15 19:10:22 -0500

    Add markview

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,9 @@
 return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = { "saghen/blink.cmp" },
+  },
   { import = "nvchad.blink.lazyspec" },
   {
     "stevearc/conform.nvim",
