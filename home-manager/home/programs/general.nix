661a506bbe277fa7b53970b64a84d932b8fbe9ac
dev-rb <43100342+dev-rb@users.noreply.github.com>
2026-01-31 21:43:30 -0500

    Add Yazi

--- home-manager/home/programs/general.nix
+++ home-manager/home/programs/general.nix
@@ -7,4 +7,16 @@
   };
 
   programs.wezterm.enableZshIntegration = true;
+
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      preview = {
+        max_width = 10000;
+        max_height = 10000;
+      };
+    };
+  };
 }
