dbb643dad9176caf0d3d0ffe4bf85bc50fcb74bc
dev-rb <43100342+dev-rb@users.noreply.github.com>
2026-01-31 21:44:55 -0500

    Clean up zsh config. Fix omp config

--- home-manager/home/programs/zsh.nix
+++ home-manager/home/programs/zsh.nix
@@ -14,24 +14,21 @@
       ignoreSpace = true;
     };
 
-    shellAliases =
-      {
-        ls = "eza --icons=always";
-        cat = "bat";
-        gcf = "git checkout $(git branch --sort=committerdate | fzf --tac --exact)";
-        gs = "git status";
-      }
-      // (
-        if vars.name == "wsl" then
-          {
+    shellAliases = {
+      ls = "eza --icons=always";
+      cat = "bat";
+    }
+    // (
+      if vars.name == "wsl" then
+        {
 
-            # Windows path for explorer
-            explorer = "/mnt/c/Windows/explorer.exe";
-            wezterm = "$WEZTERM";
-          }
-        else
-          { }
-      );
+          # Windows path for explorer
+          explorer = "/mnt/c/Windows/explorer.exe";
+          wezterm = "$WEZTERM";
+        }
+      else
+        { }
+    );
 
     defaultKeymap = "emacs";
 
@@ -46,47 +43,48 @@
       compinit -C
     '';
 
-    initContent =
-      ''
-        eval "$(oh-my-posh init zsh --config ~/dotfiles/pure.omp.json)"
-        bindkey '^p' history-search-backward
-        bindkey '^n' history-search-forward
+    initContent = ''
+      eval "$(oh-my-posh init zsh --config ~/dotfiles/pure.omp.json)"
+      bindkey '^p' history-search-backward
+      bindkey '^n' history-search-forward
 
 
-        export GOPATH="$HOME/go"
+      export GOPATH="$HOME/go"
 
-        export PATH="$HOME/.local/share/fnm:$PATH"
-        export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+      export PATH="$HOME/.local/share/fnm:$PATH"
+      export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
-        export BUN_INSTALL="$HOME/.bun"
-        export PAGER=cat
-        export PATH="$BUN_INSTALL/bin:$PATH"
+      export BUN_INSTALL="$HOME/.bun"
+      export PAGER=less
+      export BAT_PAGING=always
+      export PATH="$BUN_INSTALL/bin:$PATH"
 
-        export ANDROID_HOME="$HOME/Android/Sdk"
-        export PATH=$PATH:$ANDROID_HOME/emulator
-        export PATH=$PATH:$ANDROID_HOME/platform-tools
+      export ANDROID_HOME="$HOME/Android/Sdk"
+      export PATH=$PATH:$ANDROID_HOME/emulator
+      export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 
 
-        export PATH="$BUN_INSTALL/bin:$PATH"
+      export PATH="$BUN_INSTALL/bin:$PATH"
 
-        eval "`fnm env`"
+      eval "`fnm env`"
 
-        export PNPM_HOME="$HOME/.local/share/pnpm"
-        case ":$PATH:" in
-          *":$PNPM_HOME:"*) ;;
-          *) export PATH="$PNPM_HOME:$PATH" ;;
-        esac
+      export PNPM_HOME="$HOME/.local/share/pnpm"
+      case ":$PATH:" in
+        *":$PNPM_HOME:"*) ;;
+        *) export PATH="$PNPM_HOME:$PATH" ;;
+      esac
 
-        alias air='$(go env GOPATH)/bin/air'
-        source ~/wezterm.sh
+      alias air='$(go env GOPATH)/bin/air'
+      source ~/wezterm.sh
 
-      ''
-      + (if vars.name == "wsl" then ''export WEZTERM="$(fd wezterm.exe /mnt --max-results 1)"'' else "");
+    ''
+    + (if vars.name == "wsl" then ''export WEZTERM="$(fd wezterm.exe /mnt --max-results 1)"'' else "");
   };
 
   programs.oh-my-posh = {
     enable = true;
     enableZshIntegration = true;
+    configFile = "~/dotfiles/pure.omp.json";
   };
 }
