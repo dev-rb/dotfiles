10171c3ccce477ea02dbaf554ab50036e8edf428
dev-rb <43100342+dev-rb@users.noreply.github.com>
2026-05-16 13:58:39 -0400

    bump home stateversion.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixgl = {
       url = "github:nix-community/nixGL";
       inputs.nixpkgs.follows = "nixpkgs";
